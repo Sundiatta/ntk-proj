0c81e070a95fc6efcc24c08af47c6295bdd18a57
Sundiatta Small <sundiattasmall@gmail.com>
2020-04-28 13:49:28 -0400

    edited design of nav

---
+++ js/html-region-visuals/midwest2.js
@@ -0,0 +1,57 @@
+const particles = [];
+
+function setup(){
+    let myCanvas = createCanvas(400, 319);
+    myCanvas.parent("container");
+    for(let i = 0; i < 70; i++){
+        particles[i] = new Particles();
+    }
+}
+
+function draw(){
+    background(255);
+    particles.forEach((p, index)=>{
+        p.display();
+        p.update();
+        p.checkEdges();
+        p.checkParticles(particles.slice(index));
+    });
+}
+
+class Particles{
+    constructor(){
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(random(-2, 2), random(-2, 2));
+    }
+
+    display(){
+        noStroke();
+        noFill();
+        ellipse(this.pos.x, this.pos.y, 5, 5);
+    }
+
+    update(){
+        this.pos.add(this.vel);
+    }
+
+    checkEdges(){
+        if(this.pos.x <= 0 || this.pos.x >= width){
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y <= 0 || this.pos.y >= height){
+            this.vel.y *= -1;
+        }
+    }
+
+    checkParticles(particles){
+        particles.forEach(particle =>{
+            const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            if(d < 120){
+                stroke(122, 204, 255, 100);
+                line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            }
+        });
+    }
+}
+
---
+++ js/midwest.js
@@ -0,0 +1,56 @@
+const particles = [];
+
+function setup(){
+    let myCanvas2 = createCanvas(800, 550);
+    myCanvas2.parent("visual");
+    for(let i = 0; i < 130; i++){
+        particles[i] = new Particles();
+    }
+}
+
+function draw(){
+    background(255);
+    particles.forEach((p, index)=>{
+        p.display();
+        p.update();
+        p.checkEdges();
+        p.checkParticles(particles.slice(index));
+    });
+}
+
+class Particles{
+    constructor(){
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(random(-2, 2), random(-2, 2));
+    }
+
+    display(){
+        noStroke();
+        noFill();
+        ellipse(this.pos.x, this.pos.y, 5, 5);
+    }
+
+    update(){
+        this.pos.add(this.vel);
+    }
+
+    checkEdges(){
+        if(this.pos.x <= 0 || this.pos.x >= width){
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y <= 0 || this.pos.y >= height){
+            this.vel.y *= -1;
+        }
+    }
+
+    checkParticles(particles){
+        particles.forEach(particle =>{
+            const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            if(d < 120){
+                stroke(122, 204, 255, 100);
+                line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            }
+        });
+    }
+}
