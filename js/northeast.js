58b7bea78a1dc965c5a7e4214ab92b2846741a33
Sundiatta Small <sundiattasmall@gmail.com>
2020-04-29 11:34:33 -0400

    json issues

--- js/northeast.js
+++ js/northeast.js
@@ -1,4 +1,14 @@
 const particles = [];
+// let json;
+// let data;
+
+// function preload(){
+//     json = loadJSON("regions.json");
+// }
+
+// function getIt(){
+//     data = json.region;
+// }
 
 function setup(){
     let myCanvas2 = createCanvas(800, 550);
@@ -6,6 +16,7 @@ function setup(){
     for(let i = 0; i < 130; i++){
         particles[i] = new Particles();
     }
+    // console.log(json);
 }
 
 function draw(){
